f8b6fb6edd68cf0d4b61177c196bbe08704db54e
Benjamin Marquis <bmarquiscom@gmail.com>
2020-02-04 18:10:29 -0500

    include fix and update from phase 1

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,19 +13,68 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
-  .webpackConfig({
-    devtool: mix.inProduction() ? '' : 'inline-source-map',
-    mode: mix.inProduction() ? 'production' : 'development',
-    plugins: [
-      new BrowserSyncPlugin({
-        files: [
-          'app/**/*',
-          'public/**/*',
-          'resources/views/**/*',
-          'routes/**/*',
-        ],
-      }),
-    ],
-  });
+if (mix.inProduction()) {
+  mix.js('resources/js/app.js', 'public/js/app.js')
+    .babel('public/js/app.js', 'public/js/app.js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+      processCssUrls: false,
+    })
+    .webpackConfig({
+      devtool: mix.inProduction() ? '' : 'inline-source-map',
+      mode: mix.inProduction() ? 'production' : 'development',
+      output: {
+        chunkFilename: 'js/[name].js?id=[chunkhash]',
+      },
+      optimization: mix.inProduction() ? {} : {
+        minimize: true,
+        namedModules: true,
+        namedChunks: true,
+        removeAvailableModules: true,
+        flagIncludedChunks: true,
+        occurrenceOrder: false,
+        usedExports: true,
+        concatenateModules: true,
+        sideEffects: false,
+      },
+      plugins: [
+        new BrowserSyncPlugin({
+          files: [
+            'app/**/*',
+            'public/**/*',
+            'resources/views/**/*',
+            'routes/**/*',
+          ],
+        }),
+      ],
+      watchOptions: {
+        ignored: /node_modules/,
+      },
+    });
+} else {
+  mix.js('resources/js/app.js', 'public/js/app.js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+      processCssUrls: false,
+    })
+    .webpackConfig({
+      devtool: mix.inProduction() ? '' : 'inline-source-map',
+      mode: mix.inProduction() ? 'production' : 'development',
+      output: {
+        chunkFilename: 'js/[name].js?id=[chunkhash]',
+      },
+      plugins: [
+        new BrowserSyncPlugin({
+          files: [
+            'app/**/*',
+            'public/**/*',
+            'resources/views/**/*',
+            'routes/**/*',
+          ],
+        }),
+      ],
+      watchOptions: {
+        ignored: /node_modules/,
+      },
+    });
+}
