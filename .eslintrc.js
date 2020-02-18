f311792076c7815b69fd1c47cb7430140db6343c
Benjamin Marquis <bmarquiscom@gmail.com>
2020-02-18 15:51:34 -0500

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,61 @@ module.exports = {
     parser: "babel-eslint"
   },
   extends: [
-    "plugin:vue/recommended",
+    // "plugin:vue/recommended",
+    "@nuxtjs",
     "airbnb/base"
   ],
   plugins: [
     'eslint-plugin'
   ],
   rules: {
-    "no-extra-semi": "off",
-    "vue/require-default-prop":"off",
-    "no-mixed-spaces-and-tabs": "off",
-    "import/prefer-default-export": "off",
+    "padded-blocks": "error",
     "no-console": "off",
+    "comma-dangle": "off",
+    "newline-before-return": "error",
+    "no-extra-semi": "error",
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": "error",
+    "indent": [
+      "error",
+      2
+    ],
+    // Object rules
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ],
+    "object-curly-newline": [
+      "error",
+      {
+        "multiline": true,
+        "minProperties": 3
+      }
+    ],
+    "object-property-newline": "error",
+    "import/prefer-default-export": "off",
+    // Array rules
+    "array-bracket-spacing": [
+      "error",
+      "always"
+    ],
+    "array-bracket-newline": [
+      "error",
+      {
+        "multiline": true,
+        "minItems": 5
+      }
+    ],
+    "array-element-newline": [
+      "error",
+      {
+        "multiline": true,
+        "minItems": 5
+      }
+    ]
   },
   env : {
     "browser": true
